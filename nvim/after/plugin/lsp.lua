578cf09f108e8c548af5b1feef90e5a6bdafbbb9
Bryson Oakley <broaken13@gmail.com>
2023-06-06 21:57:42 -0600

    Add rename and code action lsp bindings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,7 +1,10 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({buffer = bufnr})
+
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = true })
 end)
 
 -- (Optional) Configure lua language server for neovim
